3fbf03df958d9034a1b9779328f99dc5253280bc
Andrei Rybak <rybak.a.v@gmail.com>
2024-01-24 00:13:30 +0100

    *.js: add and update license comments
    Add license comments to JavaScript source files, where it is missing.
    Update year range in the copyright in other files.

--- grid.js
+++ grid.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Andrei Rybak
+ * Copyright (C) 2023-2024 Andrei Rybak
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
