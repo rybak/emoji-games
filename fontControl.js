4b4405bee5c7fb685a9ba80c9b41c94622174098
Andrei Rybak <rybak.a.v@gmail.com>
2024-10-01 16:22:22 +0200

    amend! mines: add <select> for font family
    mines: add font family control

--- fontControl.js
+++ fontControl.js
@@ -1,11 +1,32 @@
 const FONT_CONTROL_CONTAINER_CLASS = "fontControlContainer";
 const FONT_CONTROL_CLASS = "fontControl";
+const FONT_FAMILIES = [
+	'OpenMoji',
+	'Noto Color Emoji',
+	'Segoe UI Emoji'
+];
 
-function createOption(sampleEmojiText, fontFamily) {
-	const o = document.createElement('option');
-	o.appendChild(document.createTextNode(sampleEmojiText));
-	o.style.fontFamily = fontFamily;
+function createOption(fontFamily) {
+	const o = document.createElement('input');
+	o.type = 'radio';
+	o.name = 'fontFamily';
+	o.id = fontFamily;
+	o.value = fontFamily;
+	o.appendChild;
+	o.addEventListener("change", (event) => {
+		event.preventDefault();
+		console.log(fontFamily);
+		document.body.style.fontFamily = fontFamily;
+	});
 	return o;
+}
+
+function createLabel(fontFamily, sampleEmojiText) {
+	const label = document.createElement('label');
+	label.htmlFor = fontFamily;
+	label.style.fontFamily = fontFamily;
+	label.append(document.createTextNode(sampleEmojiText));
+	return label;
 }
 
 function addFontControl(sampleEmojiText) {
@@ -14,14 +35,10 @@ function addFontControl(sampleEmojiText) {
 		return;
 	}
 	const container = document.querySelector(`.${FONT_CONTROL_CONTAINER_CLASS}`);
-	const select = document.createElement('select');
-	select.appendChild(createOption(sampleEmojiText, 'OpenMoji'));
-	select.appendChild(createOption(sampleEmojiText, 'Noto Color Emoji'));
-	select.appendChild(createOption(sampleEmojiText, 'Segoe UI Emoji'));
-	select.addEventListener("change", (event) => {
-		const selected = select.childNodes[select.selectedIndex];
-		console.log(selected.style.fontFamily);
-		document.body.style.fontFamily = selected.style.fontFamily;
-	});
-	container.appendChild(select);
+	const div = document.createElement('div');
+	for (const ff of FONT_FAMILIES) {
+		div.append(createOption(ff));
+		div.append(createLabel(ff, sampleEmojiText));
+	}
+	container.appendChild(div);
 }
